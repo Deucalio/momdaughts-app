3ee1c898db0ec9d3bed3d26ec72be6b96d98483a
Hamad Ali <captain.gaze@gmail.com>
2025-07-24 22:36:20 +0500

    s

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -9,7 +9,7 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="auth" options={{ headerShown: false }} />
         <Stack.Screen name="shop" options={{ headerShown: false }} />
-        <Stack.Screen name="admin" options={{ headerShown: false }} />
+        <Stack.Screen name="admin" options={{ headerShown: false }} />  
         <Stack.Screen name="products" options={{ headerShown: false }} />
       </Stack>
     </>
